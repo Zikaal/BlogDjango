2e527d7365c46eb8b0ae0a0f2a31f602cf29c604
Zikaal <168971723+Zikaal@users.noreply.github.com>
2024-08-15 00:26:49 +0500

    Add files via upload

---
+++ django_project/blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    deate_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={'pk':self.pk})
+
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
+
